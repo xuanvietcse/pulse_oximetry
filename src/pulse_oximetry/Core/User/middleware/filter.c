dd7f911e9fd9e19cedc6cca712878ce021b189aa
khanhnguyenngoc-itr-intern <ngockhanh20003@gmail.com>
2024-07-28 15:25:37 +0700

    update: Template for filter files

---
+++ src/pulse_oximetry/Core/User/middleware/filter.c
@@ -0,0 +1,35 @@
+/**
+ * @file       filter.C
+ * @copyright
+ * @license
+ * @version    1.0.0
+ * @date       28/07/2024
+ * @author     Giang Phan Truong
+ *             Khanh Nguyen Ngoc
+ *             Viet Hoang Xuan
+ *
+ * @brief      Filter for PPG signal
+ *
+ * @note       None
+ * @example    None
+ */
+
+/* Includes ----------------------------------------------------------- */
+
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+
+/* Private function prototypes ---------------------------------------- */
+
+/* Function definitions ----------------------------------------------- */
+
+/* Private definitions ------------------------------------------------ */
+
+/* End of file -------------------------------------------------------- */
---
+++ src/pulse_oximetry/Core/User/middleware/filter.h
@@ -0,0 +1,33 @@
+/**
+ * @file       filter.h
+ * @copyright
+ * @license
+ * @version    1.0.0
+ * @date       28/07/2024
+ * @author     Giang Phan Truong
+ *             Khanh Nguyen Ngoc
+ *             Viet Hoang Xuan
+ *
+ * @brief      Filter for PPG signal
+ *
+ * @note       None
+ * @example    None
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef USER_MIDDLEWARE_FILTER_H_
+#define USER_MIDDLEWARE_FILTER_H_
+
+/* Includes ----------------------------------------------------------- */
+
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+
+#endif /* USER_MIDDLEWARE_FILTER_H_ */
