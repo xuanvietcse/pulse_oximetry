eed75d1f51336f7572b1e97f95d76c476260ac9d
khanhnguyenngoc-itr-intern <ngockhanh20003@gmail.com>
2024-07-28 21:15:17 +0700

    update: Update filter function prototype comment and rename variable

--- src/pulse_oximetry/Core/User/middleware/filter.c
+++ src/pulse_oximetry/Core/User/middleware/filter.c
@@ -17,6 +17,7 @@
 /* Includes ----------------------------------------------------------- */
 #include "filter.h"
 #include "common.h"
+#include <stdint.h>
 
 /* Private defines ---------------------------------------------------- */
 
@@ -30,29 +31,29 @@
 
 // Coeffi in s-domain
 double a_s[NUM_OF_COEFFS] = {1, 
-                                28.74438523, 
-                                413.1198405, 
-                                3478.070613, 
-                                14641};
+                             28.74438523, 
+                             413.1198405, 
+                             3478.070613, 
+                             14641};
 
 double b_s[NUM_OF_COEFFS] = {0, 
-                                0, 
-                                0, 
-                                0, 
-                                14641};
+                             0, 
+                             0, 
+                             0, 
+                             14641};
 
 // Coeffi in z-domain
 double a_z[NUM_OF_COEFFS] = {1, 
-                                -3.7128, 
-                                5.1789, 
-                                -3.2162, 
-                                0.7502};
+                             -3.7128, 
+                             5.1789, 
+                             -3.2162, 
+                             0.7502};
 
 double b_z[NUM_OF_COEFFS] = {7.92609259e-06, 
-                                3.17043703e-05, 
-                                4.75565555e-05, 
-                                3.17043704e-05, 
-                                7.92609258e-06};
+                             3.17043703e-05, 
+                             4.75565555e-05, 
+                             3.17043704e-05, 
+                             7.92609258e-06};
 
 // NUM_OF_COEFFS most recent input and output
 double recent_input[NUM_OF_COEFFS] = {0};
@@ -61,23 +62,22 @@ double recent_output[NUM_OF_COEFFS] = {0};
 /* Private function prototypes ---------------------------------------- */
 
 /* Function definitions ----------------------------------------------- */
-
-// For testing, not for realtime data
-filter_status_t apply_filter(double *input_array, 
-                             double *output_array)
+filter_status_t apply_filter_to_array_of_data(double *current_input_array, 
+                                              double *current_output_array,
+                                              uint8_t size_of_array)
 {
-  __ASSERT(input_array != NULL, FILTER_ERROR);
-  __ASSERT(output_array != NULL, FILTER_ERROR);
+  __ASSERT(current_input_array != NULL, FILTER_ERROR);
+  __ASSERT(current_output_array != NULL, FILTER_ERROR);
 
-  for (int i = 0; i < 100; i++)
+  for (int i = 0; i < size_of_array; i++)
   {
-    *(output_array + i) = (b_z[0]) * (*(input_array + i));
+    *(current_output_array + i) = (b_z[0]) * (*(current_input_array + i));
     
     for (int j = 1; j < NUM_OF_COEFFS; j++)
     {
       if ((i - j) >= 0)
       {
-        *(output_array + i) += (b_z[j]) * (*(input_array + i - j));
+        *(current_output_array + i) += (b_z[j]) * (*(current_input_array + i - j));
       }
     }
 
@@ -85,7 +85,7 @@ filter_status_t apply_filter(double *input_array,
     {
       if ((i - j) >= 0)
       {
-        *(output_array + i) -= (a_z[j]) * (*(output_array + i - j));
+        *(current_output_array + i) -= (a_z[j]) * (*(current_output_array + i - j));
       }
     }
   }
@@ -94,20 +94,22 @@ filter_status_t apply_filter(double *input_array,
 }
 
 
-filter_status_t apply_filter_realtime(double *current_input_data,
-                                      double *current_output_data)
+filter_status_t apply_filter_to_single_data(double *current_input_data,
+                                            double *current_output_data)
 {
-  // Dịch các giá trị trước đó trong x và y sang phải
+  __ASSERT(current_input_data != NULL, FILTER_ERROR);
+  __ASSERT(current_output_data != NULL, FILTER_ERROR);
+
+  // Shift the recent input and output to the right
   for (int i = NUM_OF_COEFFS - 1; i > 0; --i) 
   {
     recent_input[i] = recent_input[i - 1];
     recent_output[i] = recent_output[i - 1];
   }
 
-  // Đặt giá trị hiện tại của tín hiệu đầu vào vào vị trí đầu tiên của mảng x
+  // Put the current input data to the newest position
   recent_input[0] = *current_input_data;
 
-  // Tính toán giá trị đầu ra hiện tại
   *current_output_data = b_z[0] * recent_input[0];
 
   for (int j = 1; j < NUM_OF_COEFFS; ++j) 
@@ -120,7 +122,7 @@ filter_status_t apply_filter_realtime(double *current_input_data,
       *current_output_data -= a_z[j] * recent_output[j];
   }
 
-  // Đặt giá trị đầu ra hiện tại vào vị trí đầu tiên của mảng y
+  // Put the current output data to the newest position
   recent_output[0] = *current_output_data;
 
   return FILTER_OK;
--- src/pulse_oximetry/Core/User/middleware/filter.h
+++ src/pulse_oximetry/Core/User/middleware/filter.h
@@ -39,13 +39,33 @@ typedef enum
 /* Public variables --------------------------------------------------- */
 
 /* Public function prototypes ----------------------------------------- */
+/* Public function prototypes ----------------------------------------- */
+/**
+ * @brief       Apply filter to an array of data.
+ *
+ * @param[in]   current_input_array       Pointer to the current input array of data.
+ * @param[out]  current_output_array      Pointer to the current output array of data.
+ * @param[in]   size_of_array             Size of input and output array.
+ * 
+ * @attention   Input array and output array must be the same size.
+ * 
+ * @return      The status of filter operation.
+ */
+filter_status_t apply_filter_to_array_of_data(double *current_input_array, 
+                                              double *current_output_array,
+                                              uint8_t size_of_array);
 
-// For testing, not for realtime data
-filter_status_t apply_filter(double *input_array, 
-                             double *output_array);
-
-// For realtime data
-filter_status_t apply_filter_realtime(double *current_input_data,
-                                      double *current_output_data);
+/**
+ * @brief       Apply filter to a single data.
+ *
+ * @param[in]   current_input_data       Pointer to the current input data.
+ * @param[out]  current_output_data      Pointer to the current output data.
+ * 
+ * @attention   No.
+ * 
+ * @return      The status of filter operation.
+ */
+filter_status_t apply_filter_to_single_data(double *current_input_data,
+                                            double *current_output_data);
 
 #endif /* USER_MIDDLEWARE_FILTER_H_ */
