16f4317bed12f13e29712efc52ad456220dacbca
khanhnguyenngoc-itr-intern <ngockhanh20003@gmail.com>
2024-07-29 13:10:52 +0700

    feat: Create and pass object to filter API

--- src/pulse_oximetry/Core/User/middleware/filter.c
+++ src/pulse_oximetry/Core/User/middleware/filter.c
@@ -29,63 +29,61 @@
 /* Public variables --------------------------------------------------- */
 
 /* Private variables -------------------------------------------------- */
-// Coeffi in z-domain
-double a_z[NUM_OF_COEFFS] = {0};
-double b_z[NUM_OF_COEFFS] = {0};
+// Sample coeffi in s-domain
+// double a_s[NUM_OF_COEFFS] = {1, 28.74438523, 413.1198405, 3478.070613, 14641};
+// double b_s[NUM_OF_COEFFS] = {0, 0, 0, 0, 14641};
+
+filter_t *filter_internal = NULL;
 
 /* Private function prototypes ---------------------------------------- */
 /**
  * @brief        Convert transfer function's coefficients from s-domain to z-domain.
  *
- * @param[in]    a_s      Pointer to the numerator coeffi of the transfer function in the s-domain.
- * @param[in]    b_s      Pointer to the denominator coeffi of the transfer function in the s-domain.
- * @param[out]   a_z      Pointer to the numerator coeffi of the transfer function in the z-domain.
- * @param[out]   b_z      Pointer to the denominator coeffi of the transfer function in the z-domain.
+ * @param[in]    filter      Pointer to the filter_t variable.
  * 
  * @attention    Only for 4-order butterworth filter.
  * 
  * @return       The status of filter operation.
  */
-static filter_status_t filter_btw_four_order_bilinear_transform(double *a_s, double *b_s, 
-                                                                double *a_z, double *b_z);
+static filter_status_t filter_btw_four_order_bilinear_transform(filter_t *filter);
 
 /* Function definitions ----------------------------------------------- */
-filter_status_t filter_btw_init(void *a_s, void *b_s)
+filter_status_t filter_btw_init(filter_t *filter)
 {
-  __ASSERT(a_s != NULL, FILTER_ERROR);
-  __ASSERT(b_s != NULL, FILTER_ERROR);
+  __ASSERT(filter->a_s != NULL, FILTER_ERROR);
+  __ASSERT(filter->b_s != NULL, FILTER_ERROR);
 
-  double *a_s_temp = (double *)a_s;
-  double *b_s_temp = (double *)b_s;
-  
-  uint8_t ret = filter_btw_four_order_bilinear_transform(a_s_temp, b_s_temp, a_z, b_z);
+  __ASSERT(filter->a_z != NULL, FILTER_ERROR);
+  __ASSERT(filter->b_z != NULL, FILTER_ERROR);
+
+  __ASSERT(filter->current_input_array != NULL, FILTER_ERROR);
+  __ASSERT(filter->current_output_array != NULL, FILTER_ERROR);
+
+  __ASSERT(filter->size_of_array != 0, FILTER_ERROR);
+  __ASSERT(filter->sampling_frequency != 0, FILTER_ERROR);
+
+  uint8_t ret = filter_btw_four_order_bilinear_transform(filter);
   __ASSERT(ret == FILTER_OK, FILTER_FAIL);
+
+  filter_internal = filter;
 
   return FILTER_OK;
 }
 
-filter_status_t filter_btw_process(void *current_input_array, 
-                                   void *current_output_array,
-                                   uint8_t size_of_array)
+filter_status_t filter_btw_process(filter_t *filter)
 {
-  __ASSERT(current_input_array != NULL, FILTER_ERROR);
-  __ASSERT(current_output_array != NULL, FILTER_ERROR);
+  __ASSERT(filter_internal == filter, FILTER_ERROR);
+  __ASSERT(filter_internal != NULL, FILTER_ERROR);
 
-  __ASSERT(a_z != NULL, FILTER_ERROR);
-  __ASSERT(b_z != NULL, FILTER_ERROR);
-
-  double *input_array = (double *)current_input_array;
-  double *output_array = (double *)current_output_array;
-
-  for (int i = 0; i < size_of_array; i++)
+  for (int i = 0; i < filter_internal->size_of_array; i++)
   {
-    *(output_array + i) = (b_z[0]) * (*(input_array + i));
+    filter_internal->current_output_array[i] = (filter_internal->b_z[0]) * filter_internal->current_input_array[i];
     
     for (int j = 1; j < NUM_OF_COEFFS; j++)
     {
       if ((i - j) >= 0)
       {
-        *(output_array + i) += (b_z[j]) * (*(input_array + i - j));
+        filter_internal->current_output_array[i] += (filter_internal->b_z[j]) * filter_internal->current_input_array[i - j];
       }
     }
 
@@ -93,7 +91,7 @@ filter_status_t filter_btw_process(void *current_input_array,
     {
       if ((i - j) >= 0)
       {
-        *(output_array + i) -= (a_z[j]) * (*(output_array + i - j));
+        filter_internal->current_output_array[i] -= (filter_internal->a_z[j]) * filter_internal->current_output_array[i - j];
       }
     }
   }
@@ -102,50 +100,43 @@ filter_status_t filter_btw_process(void *current_input_array,
 }
 
 /* Private definitions ------------------------------------------------ */
-static filter_status_t filter_btw_four_order_bilinear_transform(double *a_s, double *b_s, 
-                                                                double *a_z, double *b_z)
+static filter_status_t filter_btw_four_order_bilinear_transform(filter_t *filter)
 {
-  __ASSERT(a_s != NULL, FILTER_ERROR);
-  __ASSERT(b_s != NULL, FILTER_ERROR);
-
-  __ASSERT(a_z != NULL, FILTER_ERROR);
-  __ASSERT(b_z != NULL, FILTER_ERROR);
-  
   // +-1, +-2, +-4, +-6 are the coefficients obtained when expanding an algebraic identity.
 
-  double free_order_coeffi_in_b_z = (b_s[4])/(pow(2*SAMPLING_FREQUENCY, NUM_OF_COEFFS - 1));
+  double free_order_coeffi_in_b_z = (filter->b_s[4])/(pow(2*filter->sampling_frequency, NUM_OF_COEFFS - 1));
 
-  double four_order_coeffi_in_a_z = 1 + (a_s[1]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 3)) 
-                                      + (a_s[2]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 2)) 
-                                      + (a_s[3]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 1)) 
-                                      + (a_s[4]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER));
+  double four_order_coeffi_in_a_z = 1 + (filter->a_s[1]/pow((2*filter->sampling_frequency), FILTER_ORDER - 3)) 
+                                      + (filter->a_s[2]/pow((2*filter->sampling_frequency), FILTER_ORDER - 2)) 
+                                      + (filter->a_s[3]/pow((2*filter->sampling_frequency), FILTER_ORDER - 1)) 
+                                      + (filter->a_s[4]/pow((2*filter->sampling_frequency), FILTER_ORDER));
 
-  b_z[0] = free_order_coeffi_in_b_z / four_order_coeffi_in_a_z;
-  b_z[4] = free_order_coeffi_in_b_z / four_order_coeffi_in_a_z;
-  b_z[1] = (4*free_order_coeffi_in_b_z) / four_order_coeffi_in_a_z;
-  b_z[3] = (4*free_order_coeffi_in_b_z) / four_order_coeffi_in_a_z;
-  b_z[2] = (6*free_order_coeffi_in_b_z) / four_order_coeffi_in_a_z;
+  filter->b_z[0] = free_order_coeffi_in_b_z / four_order_coeffi_in_a_z;
+  filter->b_z[4] = free_order_coeffi_in_b_z / four_order_coeffi_in_a_z;
+  filter->b_z[1] = (4*free_order_coeffi_in_b_z) / four_order_coeffi_in_a_z;
+  filter->b_z[3] = (4*free_order_coeffi_in_b_z) / four_order_coeffi_in_a_z;
+  filter->b_z[2] = (6*free_order_coeffi_in_b_z) / four_order_coeffi_in_a_z;
 
-  a_z[0] = (1 + (a_s[1]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 3)) 
-              + (a_s[2]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 2)) 
-              + (a_s[3]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 1)) 
-              + (a_s[4]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER))) / four_order_coeffi_in_a_z;
+  filter->a_z[0] = (1 + (filter->a_s[1]/pow((2*filter->sampling_frequency), FILTER_ORDER - 3)) 
+                      + (filter->a_s[2]/pow((2*filter->sampling_frequency), FILTER_ORDER - 2)) 
+                      + (filter->a_s[3]/pow((2*filter->sampling_frequency), FILTER_ORDER - 1)) 
+                      + (filter->a_s[4]/pow((2*filter->sampling_frequency), FILTER_ORDER))) / four_order_coeffi_in_a_z;
 
-  a_z[1] = (-4 + -2*(a_s[1]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 3)) 
-               + 2*(a_s[3]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 1))
-               + 4*(a_s[4]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER))) / four_order_coeffi_in_a_z;
+  filter->a_z[1] = (-4 + -2*(filter->a_s[1]/pow((2*filter->sampling_frequency), FILTER_ORDER - 3)) 
+                       + 2*(filter->a_s[3]/pow((2*filter->sampling_frequency), FILTER_ORDER - 1))
+                       + 4*(filter->a_s[4]/pow((2*filter->sampling_frequency), FILTER_ORDER))) / four_order_coeffi_in_a_z;
 
-  a_z[2] = (6 + 2*(a_s[2]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 2)) 
-              + 6*(a_s[4]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER))) / four_order_coeffi_in_a_z;
+  filter->a_z[2] = (6 + 2*(filter->a_s[2]/pow((2*filter->sampling_frequency), FILTER_ORDER - 2)) 
+                      + 6*(filter->a_s[4]/pow((2*filter->sampling_frequency), FILTER_ORDER))) / four_order_coeffi_in_a_z;
 
-  a_z[3] = (-4 + 2*(a_s[1]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 3)) 
-               + 2*(a_s[3]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 1))
-               + 4*(a_s[4]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER))) / four_order_coeffi_in_a_z;
+  filter->a_z[3] = (-4 + 2*(filter->a_s[1]/pow((2*filter->sampling_frequency), FILTER_ORDER - 3)) 
+                       + 2*(filter->a_s[3]/pow((2*filter->sampling_frequency), FILTER_ORDER - 1))
+                       + 4*(filter->a_s[4]/pow((2*filter->sampling_frequency), FILTER_ORDER))) / four_order_coeffi_in_a_z;
 
-  a_z[4] = (1 - (a_s[1]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 3)) 
-              + (a_s[2]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 2))
-              - (a_s[3]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER - 1)) 
-              + (a_s[4]/pow((2*SAMPLING_FREQUENCY), FILTER_ORDER))) / four_order_coeffi_in_a_z;
+  filter->a_z[4] = (1 - (filter->a_s[1]/pow((2*filter->sampling_frequency), FILTER_ORDER - 3)) 
+                      + (filter->a_s[2]/pow((2*filter->sampling_frequency), FILTER_ORDER - 2))
+                      - (filter->a_s[3]/pow((2*filter->sampling_frequency), FILTER_ORDER - 1)) 
+                      + (filter->a_s[4]/pow((2*filter->sampling_frequency), FILTER_ORDER))) / four_order_coeffi_in_a_z;
 
   return FILTER_OK;
 }
--- src/pulse_oximetry/Core/User/middleware/filter.h
+++ src/pulse_oximetry/Core/User/middleware/filter.h
@@ -23,10 +23,12 @@
 
 /* Public defines ----------------------------------------------------- */
 #define FILTER_ORDER        (4)  
-#define NUM_OF_COEFFS       (5)   // 4-order filter has 5 coeffi       
-#define SAMPLING_FREQUENCY  (100) // Hz
+#define NUM_OF_COEFFS       (5)   // 4-order filter has 5 coefficients       
 
 /* Public enumerate/structure ----------------------------------------- */
+/**
+ * @brief Status of filter operation
+ */
 typedef enum 
 {
   FILTER_ERROR = -2,
@@ -34,39 +36,48 @@ typedef enum
   FILTER_OK
 } filter_status_t;
 
+/**
+ * @brief       Filter data type
+ * @attention   Coeffi in s-domain are provided by user or another layer (input)
+ *              Coeffi in z-domain are converted from coeffi in s-domain by the init function (output)
+ */
+typedef struct __attribute__((__packed__))
+{
+  double *a_s;                    // Pointer to the array of numerator coeffi of the transfer function in the s-domain.
+  double *b_s;                    // Pointer to the array of denominator coeffi of the transfer function in the s-domain.
+  double *a_z;                    // Pointer to the array of numerator coeffi of the transfer function in the z-domain.
+  double *b_z;                    // Pointer to the array of denominator coeffi of the transfer function in the z-domain.
+  double *current_input_array;    // Pointer to the current input array of data.
+  double *current_output_array;   // Pointer to the current output array of data.
+  uint8_t size_of_array;          // Size of input and output array.
+  uint8_t sampling_frequency;
+} filter_t;
+
 /* Public macros ------------------------------------------------------ */
 
 /* Public variables --------------------------------------------------- */
-// Coeffi in s-domain
-//double a_s[NUM_OF_COEFFS] = {1, 28.74438523, 413.1198405, 3478.070613, 14641};
-//double b_s[NUM_OF_COEFFS] = {0, 0, 0, 0, 14641};
 
 /* Public function prototypes ----------------------------------------- */
 /**
  * @brief       Init filter.
  *
- * @param[in]   a_s      Pointer to the numerator coeffi of the transfer function in the s-domain.
- * @param[in]   b_s      Pointer to the denominator coeffi of the transfer function in the s-domain.
+ * @param[in]   filter      Pointer to the filter_t variable.
  * 
  * @attention   This function should be called first.
  * 
  * @return      The status of filter operation.
  */
-filter_status_t filter_btw_init(void *a_s, void *b_s);
+filter_status_t filter_btw_init(filter_t *filter);
 
 /**
  * @brief       Apply filter to an array of data.
  *
- * @param[in]   current_input_array       Pointer to the current input array of data.
- * @param[out]  current_output_array      Pointer to the current output array of data.
- * @param[in]   size_of_array             Size of input and output array.
+ * @param[in]   filter      Pointer to the filter_t variable.
  * 
  * @attention   Input array and output array must be the same size.
  * 
  * @return      The status of filter operation.
  */
-filter_status_t filter_btw_process(void *current_input_array, 
-                                   void *current_output_array,
-                                   uint8_t size_of_array);
+filter_status_t filter_btw_process(filter_t *filter);
 
 #endif /* USER_MIDDLEWARE_FILTER_H_ */
