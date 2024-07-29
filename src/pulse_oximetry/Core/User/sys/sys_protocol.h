3b58e2c8f69cebff7815dd4c4e2172ba0a59a1ea
viethoangxuan-itr-intern <hxv1305@gmail.com>
2024-07-29 18:07:51 +0700

    chore: Create sys_protocol.h

---
+++ src/pulse_oximetry/Core/User/sys/sys_protocol.h
@@ -0,0 +1,36 @@
+/**
+ * @file       sys_protocol.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    0.0.1
+ * @date       2024-07-29
+ * @authors    Viet Hoang Xuan
+ *             Giang Phan Truong
+ *             Khanh Nguyen Ngoc
+ *
+ * @brief      <A brief description of the content of the file>
+ *
+ * @note
+ * @example
+ * @example
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __SYS_PROTOCOL_H
+#define __SYS_PROTOCOL_H
+
+/* Includes ----------------------------------------------------------- */
+
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+
+#endif // __SYS_PROTOCOL_H
+
+/* End of file -------------------------------------------------------- */
