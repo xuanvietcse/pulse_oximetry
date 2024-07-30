3f371f4ec6e9ed255d02f488f3ed126a44ca58f1
viethoangxuan-itr-intern <hxv1305@gmail.com>
2024-07-30 13:55:02 +0700

    feat: drv_ssd1306_init implemented

---
+++ src/pulse_oximetry/Core/User/drv/drv_ssd1306.c
@@ -0,0 +1,58 @@
+/**
+ * @file       drv_ssd1306.c
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    v0.0.1
+ * @date       2024-07-30
+ * @author     Viet Hoang Xuan
+ *             Giang Phan Truong
+ *             Khanh Nguyen Ngoc
+ *
+ * @brief      <A brief description of the content of the file>
+ *
+ * @note
+ * @example
+ * @example
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "drv_ssd1306.h"
+#include "common.h"
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+
+/* Private function prototypes ---------------------------------------- */
+
+/* Function definitions ----------------------------------------------- */
+uint32_t drv_ssd1306_init(drv_ssd1306_t *dev,
+                          bsp_i2c_handle_t *dev_i2c,
+                          uint16_t dev_address,
+                          uint8_t *dev_buffer,
+                          uint8_t dev_width,
+                          uint8_t dev_height)
+{
+  // Check parameters
+  __ASSERT((dev != NULL), DRV_SSD1306_ERROR);
+  __ASSERT((dev_i2c != NULL), DRV_SSD1306_ERROR);
+  __ASSERT((dev_address < 256), DRV_SSD1306_ERROR);
+  __ASSERT((dev_buffer != NULL), DRV_SSD1306_ERROR);
+  __ASSERT((dev_width <= 128), DRV_SSD1306_ERROR);
+  __ASSERT((dev_height <= 64), DRV_SSD1306_ERROR);
+  // Pass values
+  dev->i2c = dev_i2c;
+  dev->buffer = dev_buffer;
+  dev->size.width = dev_width;
+  dev->size.height = dev_height;
+  // Return
+  return DRV_SSD1306_OK;
+}
+/* Private definitions ----------------------------------------------- */
+
+/* End of file -------------------------------------------------------- */
