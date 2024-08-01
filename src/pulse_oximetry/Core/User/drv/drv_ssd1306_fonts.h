165399efefa81a6fe3fefbcaee66485d67561438
viethoangxuan-itr-intern <hxv1305@gmail.com>
2024-08-01 14:08:20 +0700

    chore: Create header file for drv_ssd1306_fonts

---
+++ src/pulse_oximetry/Core/User/drv/drv_ssd1306_fonts.h
@@ -0,0 +1,36 @@
+/**
+ * @file       drv_ssd1306_fonts.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    0.0.1
+ * @date       2024-08-01
+ * @author     Viet Hoang Xuan
+ *             Giang Phan Truong
+ *             Khanh Nguyen Ngoc
+ *
+ * @brief      <A brief description of the content of the file>
+ *
+ * @note
+ * @example
+ * @example
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __DRV_SSD1306_FONTS_H
+#define __DRV_SSD1306_FONTS_H
+
+/* Includes ----------------------------------------------------------- */
+
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+
+#endif // __DRV_SSD1306_FONTS_H
+
+/* End of file -------------------------------------------------------- */
