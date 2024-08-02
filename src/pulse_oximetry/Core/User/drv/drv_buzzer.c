42533fb135172201ce972f179b8897cf81ae0937
Giang Phan Truong <phangiang182@gmail.com>
2024-08-02 22:47:15 +0700

    update: change intialization sound effect

--- src/pulse_oximetry/Core/User/drv/drv_buzzer.c
+++ src/pulse_oximetry/Core/User/drv/drv_buzzer.c
@@ -27,7 +27,10 @@
 
 #define __DRV_BUZZER_CALCULATE_PRESCALER(__FREQUENCY__) (TIM_CLOCK_SRC / (1000 * (__FREQUENCY__)) - 1)
 /* Public variables --------------------------------------------------- */
-
+sound_effect_t m_win_11_startup[] =
+    {{NOTE_FS5, 50},
+     {NOTE_CS6, 250},
+     {NOTE_GS7, 500}};
 /* Private function prototypes ---------------------------------------- */
 
 /* Function definitions ----------------------------------------------- */
@@ -38,15 +41,13 @@ uint32_t drv_buzzer_init(drv_buzzer_t *buzzer,
   __ASSERT(buzzer != NULL, DRV_BUZZER_ERROR);
   __ASSERT(tim != NULL, DRV_BUZZER_ERROR);
 
-  uint32_t init_effect[] = {1000, 3000, 6000, 9000, 12000};
-  uint32_t note_effect_duration = 250;
-
   buzzer->config.htim = tim;
   buzzer->config.pwm_channel = pwm_channel;
   buzzer->config.prescaler = 0;
   buzzer->config.duty_cycle = 500;
   buzzer->config.period = 1000;
-  buzzer->sound_effect = init_effect;
+  buzzer->sound_effect = m_win_11_startup;
+  buzzer->note_nums = 3;
   bsp_timer_set_prescaler(buzzer->config.htim, buzzer->config.prescaler);
   bsp_timer_set_autoreload(buzzer->config.htim, buzzer->config.period);
   bsp_timer_set_output_compare(buzzer->config.htim,
@@ -59,14 +60,14 @@ uint32_t drv_buzzer_init(drv_buzzer_t *buzzer,
   uint32_t current_tick = bsp_utils_get_tick();
   do
   {
-    if (bsp_utils_get_tick() - current_tick > note_effect_duration)
+    if (bsp_utils_get_tick() - current_tick > (buzzer->sound_effect[i].duration))
     {
-      uint16_t prescaler = __DRV_BUZZER_CALCULATE_PRESCALER(buzzer->sound_effect[i]);
+      uint16_t prescaler = __DRV_BUZZER_CALCULATE_PRESCALER(buzzer->sound_effect[i].note);
       bsp_timer_set_prescaler(buzzer->config.htim, prescaler);
       current_tick = bsp_utils_get_tick();
       i++;
     }
-  } while (i < __SIZE_OF(init_effect));
+  } while (i < buzzer->note_nums);
 
   buzzer->config.prescaler = 0;
   bsp_timer_set_prescaler(buzzer->config.htim, buzzer->config.prescaler);
