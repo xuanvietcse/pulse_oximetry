f40a1cbc8c9d22ab80854fedb9eaa6a59c05cd1e
Giang Phan Truong <phangiang182@gmail.com>
2024-08-02 13:15:34 +0700

    update: define the initialization effect

--- src/pulse_oximetry/Core/User/drv/drv_buzzer.c
+++ src/pulse_oximetry/Core/User/drv/drv_buzzer.c
@@ -31,11 +31,17 @@
 /* Function definitions ----------------------------------------------- */
 uint32_t drv_buzzer_init(drv_buzzer_t *buzzer,
                          bsp_tim_typedef_t *tim,
-                         uint32_t prescaler,
-                         uint32_t pwm_channel,
-                         uint16_t period,
-                         uint16_t duty_cycle)
+                         uint32_t pwm_channel)
 {
+  __ASSERT(buzzer != NULL, DRV_BUZZER_ERROR);
+  __ASSERT(tim != NULL, DRV_BUZZER_ERROR);
+
+  buzzer->config.htim = tim;
+  buzzer->config.pwm_channel = pwm_channel;
+
+  const uint32_t init_effect[] = {1000, 3000, 6000, 9000, 12000};
+  const uint32_t note_effect_duration = 250;
+
   return DRV_BUZZER_OK;
 }
 /* Private definitions ----------------------------------------------- */
