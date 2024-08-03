8ba22c4163417ad531c4bee2d51952b76e748334
viethoangxuan-itr-intern <hxv1305@gmail.com>
2024-08-03 21:50:44 +0700

    chore: Create sys_display.c

---
+++ src/pulse_oximetry/Core/User/sys/sys_display.c
@@ -0,0 +1,37 @@
+/**
+ * @file       sys_display.c
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    0.0.1
+ * @date       2024-08-03
+ * @author     Viet Hoang Xuan
+ *             Giang Phan Truong
+ *             Khanh Nguyen Ngoc
+ *
+ * @brief      <A brief description of the content of the file>
+ *
+ * @note
+ * @example
+ * @example
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "sys_display.h"
+
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+
+/* Private function prototypes ---------------------------------------- */
+
+/* Function definitions ----------------------------------------------- */
+
+/* Private definitions ----------------------------------------------- */
+
+/* End of file -------------------------------------------------------- */
