025977a91512f31d6fed71919abd3dc771ef4d74
Giang Phan Truong <phangiang182@gmail.com>
2024-08-03 13:18:03 +0700

    update: first version of drv buzzer

--- src/pulse_oximetry/Core/User/drv/drv_buzzer.c
+++ src/pulse_oximetry/Core/User/drv/drv_buzzer.c
@@ -17,7 +17,6 @@
 /* Includes ----------------------------------------------------------- */
 #include "drv_buzzer.h"
 #include "bsp_utils.h"
-#include "list_of_sound_effects.h"
 #include "common.h"
 
 /* Private defines ---------------------------------------------------- */
@@ -26,11 +25,11 @@
 
 /* Private macros ----------------------------------------------------- */
 
-#define __DRV_BUZZER_CALCULATE_PRESCALER(__FREQUENCY__) (TIM_CLOCK_SRC / (1000 * (__FREQUENCY__)) - 1)
 /* Public variables --------------------------------------------------- */
 
 /* Private function prototypes ---------------------------------------- */
 
+static uint16_t drv_buzzer_calculate_prescaler(uint32_t frequency);
 /* Function definitions ----------------------------------------------- */
 uint32_t drv_buzzer_init(drv_buzzer_t *buzzer,
                          bsp_tim_typedef_t *tim,
@@ -39,12 +38,18 @@ uint32_t drv_buzzer_init(drv_buzzer_t *buzzer,
   __ASSERT(buzzer != NULL, DRV_BUZZER_ERROR);
   __ASSERT(tim != NULL, DRV_BUZZER_ERROR);
 
+  sound_effect_t initialization_effect[] =
+      {{NOTE_FS5, 50},
+       {NOTE_CS6, 100},
+       {NOTE_GS7, 200},
+       {NOTE_C5, 500}};
+
   buzzer->config.htim = tim;
   buzzer->config.pwm_channel = pwm_channel;
   buzzer->config.prescaler = 0;
   buzzer->config.duty_cycle = 500;
   buzzer->config.period = 1000;
-  buzzer->sound_effect = win_11_startup;
+  buzzer->sound_effect = initialization_effect;
   buzzer->note_nums = 4;
   bsp_timer_set_prescaler(buzzer->config.htim, buzzer->config.prescaler);
   bsp_timer_set_autoreload(buzzer->config.htim, buzzer->config.period);
@@ -60,7 +65,7 @@ uint32_t drv_buzzer_init(drv_buzzer_t *buzzer,
   {
     if (bsp_utils_get_tick() - current_tick > (buzzer->sound_effect[i].duration))
     {
-      uint16_t prescaler = __DRV_BUZZER_CALCULATE_PRESCALER(buzzer->sound_effect[i].note);
+      uint16_t prescaler = drv_buzzer_calculate_prescaler(buzzer->sound_effect[i].note);
       bsp_timer_set_prescaler(buzzer->config.htim, prescaler);
       current_tick = bsp_utils_get_tick();
       i++;
@@ -72,6 +77,38 @@ uint32_t drv_buzzer_init(drv_buzzer_t *buzzer,
 
   return DRV_BUZZER_OK;
 }
-/* Private definitions ----------------------------------------------- */
 
+uint32_t drv_buzzer_play(drv_buzzer_t *buzzer, sound_effect_t *sound_effect_buf, uint32_t length)
+{
+  __ASSERT(buzzer != NULL, DRV_BUZZER_ERROR);
+  __ASSERT(sound_effect_buf != NULL, DRV_BUZZER_ERROR);
+  __ASSERT(length > 0, DRV_BUZZER_ERROR);
+  __ASSERT(buzzer->active == true, DRV_BUZZER_ERROR);
+
+  buzzer->sound_effect = sound_effect_buf;
+  buzzer->note_nums = length;
+
+  uint8_t i = 0;
+  uint32_t current_tick = bsp_utils_get_tick();
+  do
+  {
+    if (bsp_utils_get_tick() - current_tick > (buzzer->sound_effect[i].duration))
+    {
+      uint16_t prescaler = drv_buzzer_calculate_prescaler(buzzer->sound_effect[i].note);
+      bsp_timer_set_prescaler(buzzer->config.htim, prescaler);
+      current_tick = bsp_utils_get_tick();
+      i++;
+    }
+  } while (i < buzzer->note_nums);
+
+  buzzer->config.prescaler = 0;
+  bsp_timer_set_prescaler(buzzer->config.htim, buzzer->config.prescaler);
+}
+/* Private definitions ----------------------------------------------- */
+static uint16_t drv_buzzer_calculate_prescaler(uint32_t frequency)
+{
+  if (frequency == 0)
+    return 0;
+  return (TIM_CLOCK_SRC / (1000 * (frequency)) - 1);
+}
 /* End of file -------------------------------------------------------- */
