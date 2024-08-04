14460e718d4b5098da771144b876800efdefe51c
Giang Phan Truong <phangiang182@gmail.com>
2024-08-04 15:10:42 +0700

    wip: apply the template

---
+++ src/pulse_oximetry/Core/User/bsp/bsp_flash.c
@@ -0,0 +1,36 @@
+/**
+ * @file       bsp_flash.c
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    v1.0.0
+ * @date       2024-08-04
+ * @author     Giang Phan Truong
+ *             Khanh Nguyen Ngoc
+ *             Viet Hoang Xuan
+ *
+ * @brief      This BSP supports to interface and transfer data to FLASH easily.
+ *
+ * @note
+ * @example    None
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "bsp_flash.h"
+
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+
+/* Private function prototypes ---------------------------------------- */
+
+/* Function definitions ----------------------------------------------- */
+
+/* Private definitions ------------------------------------------------ */
+
+/* End of file -------------------------------------------------------- */
---
+++ src/pulse_oximetry/Core/User/bsp/bsp_flash.h
@@ -0,0 +1,49 @@
+/**
+ * @file       bsp_flash.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    v1.0.0
+ * @date       2024-08-04
+ * @author     Giang Phan Truong
+ *             Khanh Nguyen Ngoc
+ *             Viet Hoang Xuan
+ *
+ * @brief      This BSP supports to interface and transfer data to FLASH easily.
+ *
+ * @note
+ * @example    None
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __USER_BSP_FLASH_H
+#define __USER_BSP_FLASH_H
+
+/* Includes ----------------------------------------------------------- */
+#include "main.h"
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+void public_function(void);
+
+#endif // __USER_BSP_FLASH_H
+
+/* End of file -------------------------------------------------------- */
