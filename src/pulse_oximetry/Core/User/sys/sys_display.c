acbeee27f4b52e10b6d9903b0e0b704148be3467
viethoangxuan-itr-intern <hxv1305@gmail.com>
2024-08-05 01:56:03 +0700

    update: Display High and Low threshold on OLED

--- src/pulse_oximetry/Core/User/sys/sys_display.c
+++ src/pulse_oximetry/Core/User/sys/sys_display.c
@@ -31,7 +31,9 @@
 
 /* Private variables -------------------------------------------------- */
 static char s_heart_rate[18] = "HeartRate: --- bpm";
-static char s_notifications[14] = "Notifications";
+static char s_notifications[5] = "Noti";
+static char s_high_threshold[6] = "H:---";
+static char s_low_threshold[6] = "L:---";
 /* Private function prototypes ---------------------------------------- */
 
 /* Function definitions ----------------------------------------------- */
@@ -50,8 +52,12 @@ uint32_t sys_display_init(sys_display_t *display, bsp_i2c_handle_t *i2c, uint8_t
   drv_ssd1306_write_string(&(display->screen), s_heart_rate, Font_6x8, DRV_SSD1306_COLOR_WHITE);
   drv_ssd1306_draw_bitmap(&(display->screen), 121, 0, heart_icon, 9, 9, DRV_SSD1306_COLOR_WHITE);
   drv_ssd1306_draw_rectangle(&(display->screen), 0, 9, 127, 54, DRV_SSD1306_COLOR_WHITE);
-  drv_ssd1306_set_cursor(&(display->screen), 25, 55);
+  drv_ssd1306_set_cursor(&(display->screen), 0, 55);
   drv_ssd1306_write_string(&(display->screen), s_notifications, Font_6x8, DRV_SSD1306_COLOR_WHITE);
+  drv_ssd1306_set_cursor(&(display->screen), 123 - 6 * 5 * 2 - 2, 55); // Set position for High threshold
+  drv_ssd1306_write_string(&(display->screen), s_high_threshold, Font_6x8, DRV_SSD1306_COLOR_WHITE);
+  drv_ssd1306_set_cursor(&(display->screen), 123 - 6 * 5, 55); // Set position for Low threshold
+  drv_ssd1306_write_string(&(display->screen), s_low_threshold, Font_6x8, DRV_SSD1306_COLOR_WHITE);
   drv_ssd1306_update_screen(&(display->screen));
   // Return
   return SYS_DISPLAY_OK;
