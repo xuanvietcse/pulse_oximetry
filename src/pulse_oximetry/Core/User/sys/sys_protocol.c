a734dc01d37d14fadf9dfc7d92c3fd30a973016d
viethoangxuan-itr-intern <hxv1305@gmail.com>
2024-08-05 21:47:47 +0700

    feat: sys_protocol_packet_cplt_handler implemented

--- src/pulse_oximetry/Core/User/sys/sys_protocol.c
+++ src/pulse_oximetry/Core/User/sys/sys_protocol.c
@@ -20,7 +20,7 @@
 #include "common.h"
 #include "drv_serial.h"
 /* Private defines ---------------------------------------------------- */
-
+#define MAX_PKT_LEN (100)
 /* Private enumerate/structure ---------------------------------------- */
 
 /* Private macros ----------------------------------------------------- */
@@ -30,9 +30,12 @@
 /* Private variables -------------------------------------------------- */
 const uint8_t s_protocol_msg[] = "\r\nStart the System Protocol finished SUCCESSFULLY!\r\n";
 static bool s_packet_cplt_flag = false;
+static uint8_t s_pkt_buf[MAX_PKT_LEN] = {0};
+static uint8_t s_pkt[MAX_PKT_LEN] = {0};
 static cbuffer_t *s_protocol_node[SYS_PROTOCOL_MAX_NODE] = {NULL};
+static uint16_t s_received_bytes = 0;
 /* Private function prototypes ---------------------------------------- */
-static void sys_protocol_receive_packet_cplt_handler();
+static void sys_protocol_receive_packet_cplt_handler(uint16_t received_bytes);
 /* Function definitions ----------------------------------------------- */
 uint32_t sys_protocol_init(UART_HandleTypeDef *uart)
 {
@@ -94,5 +97,39 @@ uint32_t sys_protocol_send_pkt_to_port(sys_protocol_pkt_t pkt)
   return SYS_PROTOCOL_OK;
 }
 /* Private definitions ----------------------------------------------- */
-
+static void sys_protocol_receive_packet_cplt_handler(uint16_t received_bytes)
+{
+  drv_serial_receive(s_pkt_buf + received_bytes);
+  s_received_bytes += received_bytes;
+  uint8_t *start_byte_pos = strchr(s_pkt_buf, 0x01);
+  uint8_t *stop_byte_pos = strchr(s_pkt_buf, 0x04);
+  if ((start_byte_pos != NULL) && (stop_byte_pos != NULL))
+  {
+    memmove(s_pkt, stop_byte_pos + 1, stop_byte_pos - start_byte_pos - 1);
+    s_packet_cplt_flag = true;
+  }
+  if (s_packet_cplt_flag)
+  {
+    // Parse pkt
+    sys_protocol_pkt_t pkt;
+    // CMD
+    pkt.command = *(s_pkt + 0) & COMMAND_FIELD;
+    // DATA
+    for (uint8_t i = 1; i < 5; i++)
+    {
+      pkt.data = *(s_pkt + i) & DATA_FIELD;
+      pkt.data <<= 1;
+    }
+    // TH Level
+    pkt.threshold_level = *(s_pkt + 5) & TH_LEVEL_FIELD;
+    // Send pkt to node
+    sys_protocol_send_pkt_to_node(SYS_PROTOCOL_SYS_MNG, pkt);
+    // Reset value
+    memset(s_pkt_buf, 0, MAX_PKT_LEN);
+    s_received_bytes = 0;
+    s_packet_cplt_flag = false;
+  }
+  // Return
+  return;
+}
 /* End of file -------------------------------------------------------- */
