81d816b17f0b8ac84e46a8f1cb771492c821dca9
viethoangxuan-itr-intern <hxv1305@gmail.com>
2024-08-05 16:34:26 +0700

    feat: sys_protocol_init implemented

--- src/pulse_oximetry/Core/User/sys/sys_protocol.c
+++ src/pulse_oximetry/Core/User/sys/sys_protocol.c
@@ -17,7 +17,8 @@
 
 /* Includes ----------------------------------------------------------- */
 #include "sys_protocol.h"
-
+#include "common.h"
+#include "drv_serial.h"
 /* Private defines ---------------------------------------------------- */
 
 /* Private enumerate/structure ---------------------------------------- */
@@ -27,11 +28,29 @@
 /* Public variables --------------------------------------------------- */
 
 /* Private variables -------------------------------------------------- */
-
+const uint8_t s_protocol_msg[] = "\r\nStart the System Protocol finished SUCCESSFULLY!\r\n";
+static bool s_packet_cplt_flag = false;
+static cbuffer_t *s_protocol_node[SYS_PROTOCOL_MAX_NODE] = {NULL};
 /* Private function prototypes ---------------------------------------- */
-
+static void sys_protocol_receive_packet_cplt_handler();
 /* Function definitions ----------------------------------------------- */
-
+uint32_t sys_protocol_init(UART_HandleTypeDef *uart)
+{
+  // Check parameters
+  __ASSERT((uart != NULL), SYS_PROTOCOL_ERROR);
+  // Operation
+  // Initialize UART
+  uint32_t ret = drv_serial_init(uart);
+  __ASSERT((ret == SYS_PROTOCOL_OK), SYS_PROTOCOL_FAILED);
+  // Register callback function
+  ret = drv_serial_register_cb_function(sys_protocol_receive_packet_cplt_handler);
+  __ASSERT((ret == SYS_PROTOCOL_OK), SYS_PROTOCOL_FAILED);
+  // Send sample message to UART to inform status
+  ret = drv_serial_transmit(s_protocol_msg, __SIZE_OF(s_protocol_msg));
+  __ASSERT((ret == SYS_PROTOCOL_OK), SYS_PROTOCOL_FAILED);
+  // Return
+  return SYS_PROTOCOL_OK;
+}
 /* Private definitions ----------------------------------------------- */
 
 /* End of file -------------------------------------------------------- */
